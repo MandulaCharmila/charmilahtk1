0bca05f86716e08d896fff595b4bb1567565fba9
Charmila Mandula <mandulacharmila@gmail.com>
2020-03-06 18:03:32 +0530

    adding the project

---
+++ src/main/java/com/charmila/charmila_collection/IndexOutOfBoundException.java
@@ -0,0 +1,10 @@
+package com.charmila.charmila_collection;
+
+public class IndexOutOfBoundException extends Exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
